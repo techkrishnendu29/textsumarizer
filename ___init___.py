dd86f1d068819b4d81a41f608342c03020100e14
Krishnendu Ghosh <krishnendughosh291004@gmail.com>
2026-04-10 02:08:12 +0530

    Update ___init___.py

--- ___init___.py
+++ ___init___.py
@@ -1,10 +1,2 @@
 # This file makes summarizer a Python package
-
-try:
-    from .summarizer import Summarizer, SummaryStyle, SummaryResult
-except ImportError:
-    # If imports fail, try alternative
-    try:
-        from summarizer import Summarizer, SummaryStyle, SummaryResult
-    except ImportError as e:
-        print(f"Warning: Could not import from summarizer module: {e}")
+pass
